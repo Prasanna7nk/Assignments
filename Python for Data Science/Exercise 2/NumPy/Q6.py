0f0146de55c8ce970190d5a842d8fe2257a1d203
Krishna Chaitanya <kchaitanya556@gmail.com>
2021-04-25 14:38:12 +0530

    Add Q6

--- Python for Data Science/Exercise 2/NumPy/Q6.py
+++ Python for Data Science/Exercise 2/NumPy/Q6.py
@@ -4,20 +4,24 @@ of 3*3 dimensions.
 """
 import numpy as np
 
-arr1 = np.array([
+A = np.array([
     [1, 2, 3],
     [2, 3, 4],
     [5, 6, 7]
 ])
 
-arr2 = np.array([
+B = np.array([
     [5, 6, 7],
     [9, 8, 2],
     [6, 6, 6]
 ])
 
-arr3 = np.array([
+C = np.array([
     [5, 3, 2],
     [0, 0, 0],
     [6, 3, 4]
 ])
+
+ABC = np.matmul(np.matmul(A, B), C)  # (A x B) x C
+
+print(ABC)
